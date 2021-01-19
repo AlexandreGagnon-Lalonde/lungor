2ffdd5d92a7708c00db10c5540c1fbbba53de8b3
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-18 21:04:59 -0500

    three main routes

---
+++ lungor/src/components/home-page/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Home() {
+  return <div>
+    <div>small explanation of application</div>
+    <div>Connect through firebase</div>
+  </div>;
+}
+
+export default Home;
---
+++ lungor/src/components/log-in-page/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function LogIn() {
+  return <div>
+    <div>small explanation of application</div>
+    <div>Connect through firebase</div>
+  </div>;
+}
+
+export default LogIn;
