da0d0d53b4238e9cd8e443a1e1f39086fd657d32
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-22 19:46:32 -0500

    submit poll form skeleton

--- src/components/home-page/index.jsx
+++ src/components/home-page/index.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
+import { SERVER_URL } from '../../constant';
+
 function Home() {
-  return <div>Home</div>;
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input type={'text'} placeholder={'Poll Name'} required />
+        </label>
+        <div>
+          <label>
+            <input type={'text'} placeholder={'Option A'} required />
+          </label>
+          <label>
+            <input type={'text'} placeholder={'Option B'} required />
+          </label>
+          <label>
+            <input type={'text'} placeholder={'Option C'} required />
+          </label>
+          <label>
+            <input type={'text'} placeholder={'Option D'} required />
+          </label>
+        </div>
+        <button type={"submit"}>Submit Poll</button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
