670b6f5b888ab55cc00b049bafbe3e240745b322
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-29 20:35:26 -0500

    remove id from data

--- src/constant.js
+++ src/constant.js
@@ -2,12 +2,10 @@ export const SERVER_URL = "http://localhost:3001";
 
 export const initialData = [
   {
-    _id: "option-0",
     optionName: "",
     voters: [],
   },
   {
-    _id: "option-1",
     optionName: "",
     voters: [],
   },
