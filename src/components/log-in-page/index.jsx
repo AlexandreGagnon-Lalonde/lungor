c15b8666bf251e4352780fe3d6684e7253960e45
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-31 20:53:09 -0500

    log in form

--- src/components/log-in-page/index.jsx
+++ src/components/log-in-page/index.jsx
@@ -1,13 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 function LogIn() {
+  const [newUser, setNewUser] = useState(false);
+  
   return (
     <div>
       <form>
         <input type={"text"} placeholder={'Username'} />
         <input type={"email"} placeholder={'Email'} />
         <input type={"password"} placeholder={'Password'} />
+        <input type={"password"} placeholder={'Verify Password'} />
         <button type={'submit'}>Sign Up</button>
+      </form>
+      <form>
+        <input type={"text"} placeholder={'Username'} />
+        <input type={"password"} placeholder={'Password'} />
+        <button type={'submit'}>Log In</button>
       </form>
     </div>
   );
