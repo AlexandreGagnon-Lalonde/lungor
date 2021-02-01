c9360d0b3f38680bd63db40d3c12ec54e470c04d
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-31 21:23:56 -0500

    handlers

--- src/components/log-in-page/index.jsx
+++ src/components/log-in-page/index.jsx
@@ -2,22 +2,48 @@ import React, { useContext, useState } from "react";
 
 function LogIn() {
   const [newUser, setNewUser] = useState(false);
+
+  const handleUserForm = (ev) => {
+    ev.preventDefault();
+    setNewUser(!newUser);
+  };
+
+  const handleLogin = () => {
+
+  }
+
+  const handleSignup = () => {
+
+  }
   
   return (
-    <div>
-      <form>
-        <input type={"text"} placeholder={'Username'} />
-        <input type={"email"} placeholder={'Email'} />
-        <input type={"password"} placeholder={'Password'} />
-        <input type={"password"} placeholder={'Verify Password'} />
-        <button type={'submit'}>Sign Up</button>
-      </form>
-      <form>
-        <input type={"text"} placeholder={'Username'} />
-        <input type={"password"} placeholder={'Password'} />
-        <button type={'submit'}>Log In</button>
-      </form>
-    </div>
+    <>
+      {newUser ? (
+        <>
+          <form>
+            <input type={"text"} placeholder={"Username"} />
+            <input type={"email"} placeholder={"Email"} />
+            <input type={"password"} placeholder={"Password"} />
+            <input type={"password"} placeholder={"Verify Password"} />
+            <button type={"submit"}>Sign Up</button>
+          </form>
+          <button type={"button"} onClick={handleUserForm}>
+            already a member?
+          </button>
+        </>
+      ) : (
+        <>
+          <form>
+            <input type={"text"} placeholder={"Username"} />
+            <input type={"password"} placeholder={"Password"} />
+            <button type={"submit"}>Log In</button>
+          </form>
+          <button type={"button"} onClick={handleUserForm}>
+            new member?
+          </button>
+        </>
+      )}
+    </>
   );
 }
 
