5f4b212493686ae40e86ae5c90ca841f8c75fa5c
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-01-31 20:52:03 -0500

    sign up form

--- src/components/log-in-page/index.jsx
+++ src/components/log-in-page/index.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 function LogIn() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <form>
+        <input type={"text"} placeholder={'Username'} />
+        <input type={"email"} placeholder={'Email'} />
+        <input type={"password"} placeholder={'Password'} />
+        <button type={'submit'}>Sign Up</button>
+      </form>
+    </div>
+  );
 }
 
 export default LogIn;
