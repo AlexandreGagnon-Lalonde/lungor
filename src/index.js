dfdf513309e2b2f3c9eea43e2d21fe6204caca6d
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-02-02 20:10:36 -0500

    wrap app with redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import GlobalStyles from './GlobalStyles';
 
+import { Provider } from "react-redux";
+import configureStore from "./reducer/store";
+
+const store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <GlobalStyles />
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
