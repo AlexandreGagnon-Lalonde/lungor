5be4ac9f0edf318b1dfcbf20f2255aaaeaa33ce7
AlexandreGagnon-Lalonde <alexandregagnonlalonde@gmail.com>
2021-02-05 21:29:33 -0500

    poll page skeleton

--- src/components/poll-page/index.jsx
+++ src/components/poll-page/index.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory, useParams, Link } from "react-router-dom";
+import { SERVER_URL, initialData } from '../../constant';
+import {
+  requestUser,
+  receiveUser,
+  userError,
+  userLogout,
+  receivePolls,
+  requestPolls,
+  pollError,
+  votePoll,
+} from "../../reducer/action";
 
 function Poll() {
-  return <div>Poll</div>;
+  const userState = useSelector(state => state.user)
+  const pollState = useSelector(state => state.poll)
+
+  const { pollId } = useParams();
+
+  const currentPoll = pollState.polls.find(poll => poll._id === pollId);
+
+  return <>
+    <div>
+      <Link to={`/`}>Back To Voting</Link>
+      <Link to={`/user/${userState.user.username}`}>{userState.user.username}</Link>
+    </div>
+    <p>{currentPoll.pollName}</p>
+    <ul>
+      {currentPoll.options.map(option => {
+        return <li>{option.optionName}</li>
+      })}
+    </ul>
+  </>;
 }
 
 export default Poll;
